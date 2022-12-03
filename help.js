52e46b488185568ab2bcefb756c1ad313c81eb96
Dilucal <armandotamba13@gmail.com>
2022-12-03 21:00:49 +0700

    bbbbbot

---
+++ help.js
@@ -0,0 +1,653 @@
+
+// disini atur aja kak menu bot nya
+// jangan lupa hapus fitur yg ga penting
+
+var monoSpace = '```'
+
+exports.listmenu = (sender,prefix,ad,namenya,premnya,usernya,romnya,tanggal,jam,no) => {
+return`
+━━━❲ 𝙂𝙐𝙍𝘽𝙊𝙏-𝙈𝘿 ━━━
+
+ 𝙂𝙍𝙊𝙐𝙋 𝙈𝙀𝙉𝙐 
+•> fitnah
+•> delete
+•> revoke
+•> tagall
+•> hidetag
+•> setdesc
+•> linkgrup
+•> infogroup
+•> setppgrup
+•> setnamegrup
+•> group open
+•> group close
+•> antilink on
+•> antilink off
+•> welcome on
+•> welcome off
+•> tiktokauto on
+•> tiktokauto off
+•> kick @tag
+•> demote @tag
+•> promote @tag
+
+ 𝙐𝙎𝙀𝙍 𝙈𝙀𝙉𝙐 
+•> verify
+•> report
+•> request
+•> transfer
+•> menfess
+•> buatroom
+•> secretchat
+•> cekprem
+•> daftarprem
+•> changename
+
+𝘼𝙉𝙊𝙉𝙔𝙈𝙊𝙐𝙎 𝙈𝙀𝙉𝙐 
+•> buatroom 628xxx
+•> room <only owner>
+•> stopchat <only room>
+•> menfess 628xx|bot|hai
+
+ 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿 𝙈𝙀𝙉𝙐
+•> tiktok
+•> ytmp3
+•> ytmp4
+•> pinterest
+•> playmp3
+•> playmp4
+•> gitclone
+•> mediafire
+•> wikimedia
+•> soundcloud
+•> infogempa
+
+ 𝙊𝙒𝙉𝙀𝙍 𝙈𝙀𝙉𝙐 
+•> error
+•> clearerr
+•> siaran
+•> session
+•> resetdb
+•> runtime
+•> setexif
+•> setwm
+•> setfooter
+•> setppbot
+•> addprem
+•> delprem
+•> bc
+•> bctext
+•> bcvideo
+•> bcaudio
+•> bcimage
+•> broadcast
+ 
+ 𝘼𝙐𝙏𝙊𝘿𝙀𝘾𝙏 𝙈𝙀𝙉𝙐 
+•> sadcat
+•> translate
+•> stalkff
+•> stalknpm
+•> stalkgithub
+•> balikhuruf
+•> balikangka
+•> besarkecil
+•> bilangangka
+
+ 𝘾𝙊𝙉𝙑𝙀𝙍𝙏 𝙈𝙀𝙉𝙐 
+•> tts
+•> ttp
+•> ttp2
+•> attp
+•> attp2
+•> tourl
+•> upload
+•> toimg
+•> toimage
+•> tomp3
+•> toaudio
+•> tomp4
+•> tovideo
+•> emojimix
+•> emojmix
+•> emojinua
+•> mixemoji
+•> stiker
+•> sticker
+•> sgif
+•> stikergif
+•> stickergif
+•> swm
+•> stikerwm
+•> stickerwm
+•> smeme
+•> memestiker
+•> stikermeme
+•> stickermeme
+•> takesticker
+•> emojinua2
+•> mixemoji2
+•> emojmix2
+•> emojimix2
+
+ 𝙏𝙊𝙊𝙇𝙎 𝙈𝙀𝙉𝙐 
+•> spamcall
+•> translate
+•> ssweb-pc
+•> ssweb-hp
+•> bitly_short
+•> cuttly_short
+•> tinyurl_short
+•> base32
+•> base64
+•> debase32
+•> debase64
+
+𝙈𝘼𝙄𝙉 𝙈𝙀𝙉𝙐 
+•> menu
+•> iklan
+•> rules
+•> owner
+•> script
+•> infobot
+•> donasi
+•> donate
+•> jadibot
+•> listjadibot
+•> sewabot
+•> groupbot
+•> ownerinfo
+•> infoowner
+
+𝙎𝙏𝙊𝙍𝙀 𝙈𝙀𝙉𝙐 
+•> kali 1 2
+•> bagi 1 2
+•> kurang 1 2
+•> tambah 1 2
+•> dellist key
+•> addlist key@response
+•> update key@response
+•> done <reply orderan>
+•> proses <reply orderan>
+•> list <only group>
+•> shop <only group>
+
+ 𝘽𝙐𝙂𝙑𝙄𝙋 𝙈𝙀𝙉𝙐 
+•> sendbug 628xxx
+•> philips 628xxx
+•> philips2 628xxx
+•> philips3 628xxx
+•> santet @tag
+•> santet2 @tag
+•> santet3 @tag
+•> virtex 628xxx
+•> virtex2 628xxx
+•> virtex3 628xxx
+•> bug1 628xxx
+•> bug2 628xxx
+•> bug3 628xxx
+•> bug4 628xxx
+•> bug5 628xxx
+ 
+ 𝙍𝘼𝙉𝘿𝙊𝙈 𝘾𝙀𝙆 
+•> cekjelek
+•> cekcantik
+•> cekganteng
+•> ceksad
+•> cekharam
+•> cekhalal
+•> cekbego
+•> cekanjing
+•> cekbiadab
+•> cekramah
+•> ceksatir
+•> cekmanis
+•> cekpahit
+•> cekhitam
+•> cekrasis
+•> cekbaik
+•> cekjahat
+•> cekkaya
+•> cekmiskin
+•> cekpintar
+•> cekbodoh
+•> cekimut
+•> cekkocak
+•> cekkadang
+
+ 𝙎𝙀𝙍𝙏𝙄𝙁𝙄𝙆𝘼𝙏 𝙈𝙀𝙉𝙐 
+•> toloserti <nama>
+•> badgirlserti <nama>
+•> goodgirlserti <nama>
+•> fuckgirlserti <nama>
+•> bucinserti <nama>
+ 
+ 𝙇𝙊𝙂𝙊 𝙈𝘼𝙆𝙀𝙍 
+•> joker
+•> digital
+•> nulis
+•> nulis2
+•> quoteser
+•> quobucin
+•> rem
+•> girlneko
+•> sadboy
+•> kaneki
+•> lolimaker
+
+ 𝙎𝙊𝙎𝙈𝙀𝘿 𝙎𝙃𝙊𝙋 
+•> pricelist <layanan>
+•> order <cara order>
+•> like jumlah|target
+•> view jumlah|target
+•> follower jumlah|username
+•> cekstatus <idtrx>
+•> komisi <owner only>
+•> tarikkomisi <owner only>
+ 
+ 𝙑𝙊𝙆𝘼𝙇 𝙈𝙀𝙉𝙐 
+•> hilih <text>
+•> halah <text>
+•> huluh <text>
+•> heleh <text>
+•> holoh <text>
+ 
+ 𝙍𝘼𝙉𝘿𝙊𝙈 𝙈𝙀𝙉𝙐 
+•> cecan
+•> cogan
+•> mobil
+•> islamic
+•> darkjokes
+•> boneka
+•> wallhp
+•> tatasurya
+•> programing
+
+ 𝙋𝙍𝙄𝙈𝘽𝙊𝙉 𝙈𝙀𝙉𝙐 
+•> ramaljodoh
+•> ramalanjodoh
+•> nomorhoki
+•> artimimpi
+•> artinama
+•> sifatusaha
+•> tafsirmimpi
+•> pasangan
+•> suamiistri
+•> ramalcinta
+•> ramalancinta
+•> ramaljodohbali
+•> ramalanjodohbali
+•> cocoknama
+•> kecocokannama
+•> cocokpasangan
+•> kecocokanpasangan
+
+ 𝘼𝙎𝙐𝙋𝘼𝙉 𝙂𝘼𝘾𝙃𝘼 
+•> rika
+•> bocil
+•> ukhty
+•> santuy
+•> hijaber
+
+ 𝘼𝙐𝘿𝙄𝙊 𝘾𝙃𝘼𝙉𝙂𝙀𝙍 
+•> fat
+•> fast
+•> slow
+•> bass
+•> deep
+•> tupai
+•> robot
+•> blown
+•> smooth
+•> earrape
+•> reverse
+•> nightcore
+
+ 𝙄𝙉𝙁𝙊𝙍𝙈𝘼𝙏𝙄𝙊𝙉 𝙈𝙀𝙉𝙐 
+•> gempa
+•> jadwaltv
+•> gempanow
+•> bioskopnow
+•> latintoaksara
+•> aksaratolatin
+
+ 𝙍𝘼𝙉𝘿𝙊𝙈 𝙎𝙏𝙄𝘾𝙆𝙀𝙍 
+•> dadu
+•> anjing
+•> patrick
+•> bucinstick
+•> gawrgura
+•> amongus
+
+ 𝘼𝙐𝘿𝙄𝙊 𝙈𝙀𝙉𝙐 
+•> audio${ad++}
+ 
+ 𝙁𝙐𝙉 𝙈𝙀𝙉𝙐 
+•> goblokcek 
+•> jelekcek 
+•> gaycek
+•> lesbicek
+•> gantengcek 
+•> cantikcek
+•> begocek 
+•> suhucek
+•> pintercek
+•> jagocek
+•> nolepcek
+•> babicek
+•> bebancek
+•> baikcek
+•> jahatcek
+•> anjingcek
+•> haramcek
+•> pakboycek
+•> pakgirlcek
+•> sangecek 
+•> bapercek
+•> fakboycek
+•> alimcek
+•> suhucek
+•> fakgirlcek
+•> kerencek
+•> wibucek
+
+ 𝙒𝘼𝙇𝙇𝙋𝘼𝙋𝙀𝙍 𝙈𝙀𝙉𝙐 
+•> milf
+•> loli
+•> wallml
+•> waifu
+•> husbu
+•> cosplay
+•> ppcouple
+•> wallpaperislami
+•> wallpaperinori
+•> wallpaperanime
+•> wallpapergamer
+•> wallpapermeme
+•> wallpaperprogamer
+•> wallpaperteknologi
+•> wallpapercyber
+
+ 𝘼𝙉𝙄𝙈𝙀 𝙈𝙀𝙉𝙐 
+•> cry
+•> hug
+•> pat
+•> bully
+•> lick
+•> kiss
+•> awoo
+•> waifu
+•> shinobu
+•> cuddle
+•> megumin
+•> slap
+•> neko
+•> wink
+•> dance
+•> poke
+•> glomp
+•> bite
+•> nom
+•> handhold
+•> highfive
+•> wave
+•> smug
+•> smile
+•> bonk
+
+ 𝘾𝙀𝙍𝙋𝙀𝙉 𝙈𝙀𝙉𝙐 
+•> cerpen-sejarah
+•> cerpen-sedih
+•> cerpen-sastra
+•> cerpen-romantis
+•> cerpen-rohani
+•> cerpen-rindu
+•> cerpen-remaja
+•> cerpen-ramadhan
+•> cerpen-petualangan
+•> cerpen-persahabatan
+•> cerpen-perpisahan
+•> cerpen-perjuangan
+•> cerpen-penyesalan
+•> cerpen-pengorbanan
+•> cerpen-pengalaman
+•> cerpen-pendidikan
+•> cerpen-penantian
+•> cerpen-patahhati
+•> cerpen-olahraga
+•> cerpen-nasionalisme
+•> cerpen-nasihat
+•> cerpen-motivasi
+•> cerpen-misteri
+•> cerpen-mengharukan
+•> cerpen-malaysia
+•> cerpen-liburan
+•> cerpen-kristen
+•> cerpen-korea
+•> cerpen-kisahnyata
+•> cerpen-keluarga
+•> cerpen-kehidupan
+•> cerpen-jepang
+•> cerpen-inspiratif
+•> cerpen-gokil
+•> cerpen-galau
+•> cerpen-cintasejati
+•> cerpen-cintasegitiga
+•> cerpen-cintasedih
+•> cerpen-cintaromantis
+•> cerpen-cintapertama
+•> cerpen-cintaislami
+•> cerpen-cinta
+•> cerpen-budaya
+•> cerpen-bahasasunda
+•> cerpen-bahasajawa
+•> cerpen-bahasainggris
+•> cerpen-bahasadaerah
+•> cerpen-anak
+
+ 𝙉𝙎𝙁𝙒 𝙈𝙀𝙉𝙐 
+•> yuri
+•> ero
+•> foot
+•> ass
+•> cuckold
+•> cum
+•> hentai
+•> gangbang
+•> femdom
+•> ahegao
+•> glasses
+•> jahy
+•> orgy
+•> pussy
+•> thighs
+•> panties
+•> thighs
+•> masturbation
+•> chiisaihentai
+•> trap
+•> blowjob
+•> yaoi
+•> ecchi
+•> hentai
+•> ahegao
+•> hololewd
+•> sideoppai
+•> animefeets
+•> animebooty
+•> animethighss
+•> hentaiparadise
+•> animearmpits
+•> hentaifemdom
+•> lewdanimegirls
+•> biganimetiddies
+•> animebellybutton
+•> hentai4everyone
+
+ 𝙎𝙊𝙐𝙉𝘿 𝙈𝙀𝙉𝙐 
+•> sound1 - sound100
+
+ 𝙏𝙀𝙓𝙏𝙋𝙍𝙊 𝙈𝙀𝙉𝙐 
+•> metallic text
+•> naruto text
+•> butterfly text
+•> flaming text
+•> shadow text
+•> cup text
+•> cup1 text
+•> romance text
+•> smoke text
+•> burnpaper text
+•> lovemessage text
+•> undergrass text
+•> love text
+•> coffe text
+•> woodheart text
+•> woodenboard text
+•> summer3d text
+•> wolfmetal text
+•> nature3d text
+•> underwater text
+•> goldenrose text
+•> summernature text
+•> letterleaves text
+•> glowingneon text
+•> fallleaves text
+•> flamming text
+•> harrypotter text
+•> carvedwood text
+•> tiktok text1 text2
+•> arcade8bit text1 text2
+•> battlefield4 text1 text2
+•> pubg text1 text2
+
+ 𝙀𝙋𝙃𝙊𝙏𝙊 𝙈𝙀𝙉𝙐 
+•> wetglass text
+•> multicolor3d text
+•> watercolor text
+•> luxurygold text
+•> galaxywallpaper text
+•> lighttext text
+•> beautifulflower text
+•> puppycute text
+•> royaltext text
+•> heartshaped text
+•> birthdaycake text
+•> galaxystyle text
+•> hologram3d text
+•> greenneon text
+•> glossychrome text
+•> greenbush text
+•> metallogo text
+•> noeltext text
+•> glittergold text
+•> textcake text
+•> starsnight text
+•> wooden3d text
+•> textbyname text
+•> writegalacy text
+•> galaxybat text
+•> snow3d text
+•> birthdayday text
+•> goldplaybutton text
+•> silverplaybutton text
+•> freefire text
+
+ 𝙍𝘼𝙉𝘿𝙊𝙈 𝙄𝙈𝘼𝙂𝙀 
+•> bj
+•> ero
+•> ppcp
+•> cum
+•> feet
+•> yuri
+•> trap
+•> lewd
+•> feed
+•> eron
+•> solo
+•> gasm
+•> poke
+•> anal
+•> holo
+•> tits
+•> kuni
+•> kiss
+•> erok
+•> smug
+•> baka
+•> solog
+•> feetg
+•> lewdk
+•> waifu
+•> pussy
+•> femdom
+•> cuddle
+•> hentai
+•> eroyuri
+•> cum_jpg
+•> blowjob
+•> erofeet
+•> holoero
+•> classic
+•> erokemo
+•> fox_girl
+•> futanari
+•> lewdkemo
+•> wallpaper
+•> pussy_jpg
+•> kemonomimi
+•> nsfw_avatar
+ `
+}
+
+exports.rulesBot = () =>{
+return`*──「 RULES-BOT 」──*
+
+1. Jangan spam bot. 
+Sanksi: *WARN/SOFT BLOCK*
+
+2. Jangan telepon bot.
+Sanksi: *SOFT BLOCK*
+
+3. Jangan mengejek bot.
+Sanksi: *PERMANENT BLOCK*
+
+Jika sudah paham rulesnya
+Ketik *#menu* untuk memulai bot`
+}
+
+exports.donasiBot = (cekName,ucapanWaktu) =>{
+return`──「 *MENU DONATE* 」──
+
+Hi *${cekName}* ${ucapanWaktu} 👋🏻
+
+*Payment Ovo*
+Number: 085789004732
+A/N: LEXXY STORE
+
+*Payment Dana*
+Number: 085789004732
+A/N: LEXXY OFFICIAL
+
+${monoSpace}Terimakasih untuk kamu yang sudah donasi untuk perkembangan bot ini ^_^${monoSpace}
+
+──「 *THX FOR YOU* 」──`
+}
+
+exports.infoOwner = () =>{
+return`──「 *INFO OWNER* 」──
+
+ *Data Profil*
+ • *Nama:* Lexxy Official
+ • *Umur:* 18 tahun
+ • *Hoby:* Turu/Game
+ • *Askot:* Palembang
+ • *Konten:* Creator
+
+_iam developer bot whatsapp._
+
+ *Sosial Media*
+ • *Whatsapp:* 083834558105
+ • *Youtube:* Lexxy Official
+ • *Github:* Lexxy24
+ `
+}
